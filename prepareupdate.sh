6df8359ffc3c81a72ddf386654a4c21fb3f797ea
xdevs23 <xdevs23@outlook.com>
2016-03-25 19:03:50 +0100

    Stable 1.0.2

--- prepareupdate.sh
+++ prepareupdate.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-APPRELPATH="app/build/outputs/apk/app-release.apk"
+APPTYPE="theme"
+APPRELPATH="$APPTYPE/build/outputs/apk/$APPTYPE-release.apk"
 APPNAME="BluEmerald"
-UPDATEABLE=false
+UPDATEABLE="false"
 
-if [ -z $1 ]; then echo "Usage: $0 vername vercode vertype"
+if [ -z $1 ]; then echo "Usage: source prepareupdate.sh vername vercode vertype"
 else
   if [ ! "$4" == "--updateOnly" ]; then source make.sh; fi
-  if [ UPDATEABLE ]; then cp "$APPRELPATH" "update/$APPNAME.apk"; fi
+  if [ $UPDATEABLE == "true" ]; then cp "$APPRELPATH" "update/$APPNAME.apk"; fi
   cp "$APPRELPATH" "versions/$APPNAME-$1-$2$3.apk"
 
-  if [ UPDATEABLE ]; then
+  if [ $UPDATEABLE == "true" ]; then
     echo -en $1>update/version.txt
     echo -en $2>update/rel.txt
 
