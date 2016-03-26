fb403246c67dac8f5cca66e33d2d78efddfc8eef
xdevs23 <xdevs23@outlook.com>
2016-03-26 14:58:17 +0100

    Transform CM theme to Layers theme [1/?]

---
+++ makescripts/99cleanup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "Cleaning up..."
+
+FIRSTDIR=$(pwd)
+
+if [ ! "${PWD##*/}" == "makescripts" ]; then cd makescripts; fi
+
+ALLSCRIPTS=$(ls)
+
+for word in $ALLSCRIPTS; do
+  if [ ! $word == "99cleanup.sh" ]; then
+    ./$word c
+  fi
+done
+
+cd $FIRSTDIR
+
+echo "Done!"
+
